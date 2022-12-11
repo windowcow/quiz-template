dcbba9a542388142f453daa5d2e4bd505df5b6ea
windowcow <65646883+windowcow@users.noreply.github.com>
2022-12-11 18:32:41 +0900

    ㅁㄴㅇㄹ

--- index.js
+++ index.js
@@ -115,12 +115,12 @@ function submitClicked(event) {
         else {
             var wrongDeco = '<div class="col-1 m-auto fw-bold text-danger text-center">WRONG</div>';
             $(element).parent().prepend(wrongDeco);
-            $(element).removeClass('btn-primary');
-            $(element).removeClass('btn-outline-primary');
-            $(element).toggle('text-white');
-            $(element).addClass('btn-outline-danger');
-            $(element).append('-> THIS IS A WRONG CHOICE')
-            element.disabled = true;
+            $(element).remove();
+            // $(element).removeClass('btn-primary');
+            // $(element).removeClass('btn-outline-primary');
+            // $(element).toggle('text-white');
+            // $(element).addClass('btn-outline-danger');
+            // $(element).append('-> THIS IS A WRONG CHOICE')
             event.target.disabled = true;
 
         }
